07acde4295cee440dabea370e0b099aaa79d0f46
Sergei Makarenko <ItSerzh@gmail.com>
2025-07-16 08:51:11 +0300

    Add Product endpoint minimal Api with carter

---
+++ src/BuildingBlocks/BuildingBlocks/IBuildingBlocksMarker.cs
@@ -0,0 +1,6 @@
+﻿namespace BuildingBlocks;
+
+public interface IBuildingBlocksMarker
+{
+}
+
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,23 @@
+using BuildingBlocks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
 // services here
+builder.Services.AddCarter(new DependencyContextAssemblyCatalog([
+    Assembly.GetExecutingAssembly(),                       // Your main app
+    typeof(IBuildingBlocksMarker).Assembly                 // External module assembly
+]));
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 //  HTTP pipeline here
-
+app.MapCarter();
 app.Run();
