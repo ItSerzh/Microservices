153bb20d3f1ed34aa105100953e84abedfc6895e
Sergei Makarenko <ItSerzh@gmail.com>
2025-07-28 09:06:18 +0300

    Ordering ms: Develop DDD abstractions

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Entity.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Ordering.Domain.Abstractions;
+
+public abstract class Entity<T> : IEntity<T>
+{
+    public T Id { get; set; } = default!;
+    public DateTimeOffset CreatedAt { get; set; }
+    public string CreatedBy { get; set; } = default!;
+    public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.UtcNow;
+    public string UpdatedBy { get; set; } = default!;
+}
+
---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstractions;
+
+public interface IDomainEvent : INotification
+{
+    Guid EventId => Guid.NewGuid();
+
+    DateTimeOffset HappendOn => DateTimeOffset.UtcNow;
+
+    string EventType => GetType().AssemblyQualifiedName;
+}
+
