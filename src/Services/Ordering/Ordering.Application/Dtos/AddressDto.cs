66e8113863f4c8284306062ff5b73a236b285efd
Sergei Makarenko <ItSerzh@gmail.com>
2025-07-31 17:24:29 +0300

    Ordering ms: Create Dtos

---
+++ src/Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Application.Dtos;
+
+public record AddressDto(
+    string FirstName,
+    string LastName,
+    string? EmailAddress,
+    string AddressLine, 
+    string Contry,
+    string State,
+    string ZipCode);
+
---
+++ src/Services/Ordering/Ordering.Application/Dtos/OrderDto.cs
@@ -0,0 +1,14 @@
+﻿using Ordering.Domain.Enums;
+
+namespace Ordering.Application.Dtos;
+
+public record class OrderDto(
+    Guid Id,
+    Guid CustomerId,
+    string OrderName,
+    AddressDto Shipping,
+    AddressDto Billing,
+    PaymentDto Payment,
+    OrderStatus Status,
+    List<OrderItemDto> OrderItems);
+
