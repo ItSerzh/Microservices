9b1ebbcf05e05133a0363579d0bd311da03f9eda
Sergei Makarenko <ItSerzh@gmail.com>
2025-08-01 12:36:54 +0300

    Ordering ms: Develop Event Handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Events/Domain/OrderCreatedEventHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Application.Orders.Events.Domain;
+
+public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"Handled {notification.GetType().Name}");
+
+        return Task.CompletedTask;
+    }
+}
+
---
+++ src/Services/Ordering/Ordering.Application/Orders/Events/Domain/OrderUpdatedEventHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Application.Orders.Events.Domain;
+
+public class OrderUpdatedEventHandler(ILogger<OrderUpdatedEventHandler> logger)
+    : INotificationHandler<OrderUpdatedEvent>
+{
+    public Task Handle(OrderUpdatedEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"Handled {notification.GetType().Name}");
+        
+        return Task.CompletedTask;
+    }
+}
+
