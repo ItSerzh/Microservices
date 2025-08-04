0ea6b7b8f43661ea7560112981e0f4d9a2742581
Sergei Makarenko <ItSerzh@gmail.com>
2025-08-04 13:50:50 +0300

    Ordering ms: Add Health checks and Custom exceptions hadling

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,15 +1,23 @@
-﻿using BuildingBlocks.Extensions;
+﻿using BuildingBlocks.Exceptions.Handler;
+using BuildingBlocks.Extensions;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using System.Reflection;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
     {
         var currentAssembly = Assembly.GetExecutingAssembly();
 
         services.AddCarterForAssembly(currentAssembly);
+
+        services.AddExceptionHandler<CustomExceptionHandler>();
+
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
 
         return services;
     }
@@ -17,6 +25,14 @@ public static class DependencyInjection
     public static WebApplication UseApiServices(this WebApplication app)
     {
         app.MapCarter();
+
+        app.UseExceptionHandler(opts => { });
+
+        app.UseHealthChecks("/health",
+            new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
 
         return app;
     }
